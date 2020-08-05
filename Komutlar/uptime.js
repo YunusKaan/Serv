b98aa1976bc5d54624f41c9036ad34259518130c
YunusKaan <69267043+YunusKaan@users.noreply.github.com>
2020-08-05 23:17:09 +0300

    Create uptime.js

---
+++ Komutlar/uptime.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json");
+const express = require('express');
+const http = require('http');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 60000);
